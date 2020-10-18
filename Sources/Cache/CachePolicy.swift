b43cd7e0dd100b3a0960cf3544b463366367f3f5
Vincent Esche <regexident@gmail.com>
2020-10-18 03:10:00 +0200

    Moved linked list into pre-allocated array with index-based links

--- Sources/Cache/CachePolicy.swift
+++ Sources/Cache/CachePolicy.swift
@@ -5,16 +5,32 @@
 import Foundation
 
 public protocol CachePolicy {
+    /// The policy's token type.
     associatedtype Token: Hashable
 
+    /// Creates an empty policy.
     init()
+
+    /// Creates an empty policy with preallocated space
+    /// for at least the specified number of tokens.
     init(minimumCapacity: Int)
 
+    /// Inserts a new token into the policy.
     mutating func insert() -> Token
 
+    /// Marks a token as used.
     mutating func use(_ token: Token)
 
-    mutating func remove() -> Token?
+    /// Returns the best token to be removed next
+    mutating func next() -> Token?
+
+    /// Removed a token.
     mutating func remove(_ token: Token)
+
+    /// Removes all tokens from the policy.
+    mutating func removeAll()
+
+    /// Removes all tokens from the policy.
+    /// - Parameter keepCapacity: Pass `true` to keep the existing capacity of the policy after removing its tokens.
     mutating func removeAll(keepingCapacity keepCapacity: Bool)
 }
--- Sources/Cache/LRU/LRUQueue.swift
+++ Sources/Cache/LRU/LRUQueue.swift
@@ -4,100 +4,219 @@
 
 import Foundation
 
-internal class LRUQueue {
-    internal class Node: Equatable {
-        internal var next: Node?
-        internal weak var previous: Node?
+internal struct LRUQueue {
+    internal typealias Index = Int
 
-        internal init(
-            next: Node? = nil,
-            previous: Node? = nil
-        ) {
-            self.next = next
-            self.previous = previous
+    internal enum Node: Equatable {
+        internal struct Free: Equatable {
+            var nextFree: Int?
         }
 
-        internal static func == (
-            lhs: Node,
-            rhs: Node
-        ) -> Bool {
-            ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+        internal struct Occupied: Equatable {
+            var previous: Index?
+            var next: Index?
         }
+
+        case free(Free)
+        case occupied(Occupied)
     }
 
-    internal private(set) var head: Node?
-    internal private(set) var tail: Node?
+    internal private(set) var head: Index?
+    internal private(set) var tail: Index?
+    internal private(set) var nodes: [Node]
+    internal private(set) var free: Index?
 
     internal init() {
-        self.head = nil
-        self.tail = nil
+        self.init(minimumCapacity: 0)
     }
 
-    internal func enqueue() -> Node {
-        let node = Node()
-        self.enqueue(node)
-        return node
+    internal init(minimumCapacity: Int) {
+        assert(minimumCapacity >= 0)
+
+        self.init(
+            head: nil,
+            tail: nil,
+            nodes: (0..<minimumCapacity).map { index in
+                let nextIndex = index + 1
+                let nextFree = (nextIndex < minimumCapacity) ? nextIndex : nil
+                return .free(.init(nextFree: nextFree))
+            },
+            free: (minimumCapacity > 0) ? 0 : nil
+        )
     }
 
-    internal func enqueue(_ node: Node) {
-        assert(node.previous == nil)
-        assert(node.next == nil)
+    internal init(
+        head: Index?,
+        tail: Index?,
+        nodes: [Node],
+        free: Index?
+    ) {
+        self.head = head
+        self.tail = tail
+        self.nodes = nodes
+        self.free = free
+    }
 
-        guard self.head != nil else {
-            self.head = node
-            self.tail = node
+    internal mutating func enqueue() -> Int {
+        let index: Index
+        if let free = self.free {
+            index = free
+            self.free = self.freeNode(at: index).nextFree
+        } else {
+            index = self.nodes.count
+            self.nodes.append(.occupied(.init()))
+        }
+        self.enqueue(index)
+        return index
+    }
 
+    internal mutating func enqueue(_ index: Int) {
+        let currentHead = self.head
+
+        let free: Index? = self.modifyNode(at: index) { node in
+            defer {
+                node = .occupied(.init(
+                    previous: nil,
+                    next: currentHead
+                ))
+            }
+
+            guard case .free(let free) = node else {
+                return nil
+            }
+            return free.nextFree
+        }
+
+        if let head = currentHead {
+            self.modifyOccupiedNode(at: head) { occupied in
+                occupied.previous = index
+            }
+        } else {
+            self.tail = index
+        }
+        self.head = index
+        self.free = free
+    }
+
+    internal mutating func next() -> Index? {
+        self.tail
+    }
+
+    internal mutating func dequeue(_ index: Index) {
+        let nodeOrNil: Node.Occupied? = self.modifyNode(at: index) { node in
+            switch node {
+            case .free(_):
+                return nil
+            case .occupied(let occupied):
+                node = .occupied(.init())
+                return occupied
+            }
+        }
+
+        guard let node = nodeOrNil else {
             return
         }
 
-        node.previous = nil
-
-        let currentHead = self.head
-
-        self.head?.previous = node
-        self.head = node
-        self.head?.next = currentHead
-    }
-
-    internal func dequeue(_ node: Node) {
-        if self.head === node {
+        if self.head == index {
             self.head = node.next
         }
-        if self.tail === node {
+
+        if self.tail == index {
             self.tail = node.previous
         }
 
-        if let previousNode = node.previous {
-            previousNode.next = node.next
+        if let previousIndex = node.previous {
+            self.modifyOccupiedNode(at: previousIndex) { occupied in
+                assert(occupied.next == index)
+                occupied.next = node.next
+            }
         }
-        if let nextNode = node.next {
-            nextNode.previous = node.previous
+        if let nextIndex = node.next {
+            self.modifyOccupiedNode(at: nextIndex) { occupied in
+                assert(occupied.previous == index)
+                occupied.previous = node.previous
+            }
         }
 
-        node.next = nil
-        node.previous = nil
+        self.nodes[index] = .free(.init(nextFree: self.free))
+        self.free = index
     }
 
-    @discardableResult
-    internal func dequeueLeastRecentlyUsed() -> Node {
-        guard let tail = self.tail else {
-            fatalError(
-                "Can't remove last node from an empty queue"
-            )
-        }
-
-        self.dequeue(tail)
-
-        return tail
-    }
-
-    internal func dequeueAll() {
+    internal mutating func dequeueAll(
+        keepingCapacity keepCapacity: Bool = false
+    ) {
         self.head = nil
         self.tail = nil
+
+        self.nodes.removeAll(keepingCapacity: keepCapacity)
+        self.free = nil
     }
 
-    internal func requeue(_ node: Node) {
-        self.dequeue(node)
-        self.enqueue(node)
+    internal mutating func requeue(_ index: Index) {
+        guard self.head != index else {
+            return
+        }
+
+        self.dequeue(index)
+        self.enqueue(index)
+    }
+
+    private func occupiedNode(at index: Index) -> Node.Occupied {
+        let node = self.nodes[index]
+        guard case .occupied(let occupied) = node else {
+            fatalError("Expected occupied slot, found free.")
+        }
+        return occupied
+    }
+
+    private func freeNode(at index: Index) -> Node.Free {
+        let node = self.nodes[index]
+        guard case .free(let free) = node else {
+            fatalError("Expected free slot, found occupied.")
+        }
+        return free
+    }
+
+    private mutating func modifyOccupiedNode<T>(
+        at index: Index,
+        _ closure: (inout Node.Occupied) -> T
+    ) -> T {
+        self.modifyNode(at: index) { node in
+            guard case .occupied(var occupied) = node else {
+                fatalError("Expected occupied slot, found free.")
+            }
+
+            defer {
+                node = .occupied(occupied)
+            }
+
+            return closure(&occupied)
+        }
+    }
+
+    private mutating func modifyFreeNode<T>(
+        at index: Index,
+        _ closure: (inout Node.Free) -> T
+    ) -> T {
+        self.modifyNode(at: index) { node in
+            guard case .free(var free) = node else {
+                fatalError("Expected free slot, found occupied.")
+            }
+
+            defer {
+                node = .free(free)
+            }
+
+            return closure(&free)
+        }
+    }
+
+    private mutating func modifyNode<T>(
+        at index: Index,
+        _ closure: (inout Node) -> T
+    ) -> T {
+        self.nodes.modifyElement(at: index) { node in
+            return closure(&node)
+        }
     }
 }
