f97312ad2b93bcff2ea72a2b0822fca0962a9dbb
Vincent Esche <regexident@gmail.com>
2020-10-18 21:20:30 +0200

    Moved convenience extensions of `Cache` into file 'Cache+Extensions.swift'

---
+++ Sources/Cache/Cache+Extensions.swift
@@ -0,0 +1,161 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Foundation
+
+extension Cache
+where
+    Cost == Int
+{
+    /// The default cost associated with all stored value,
+    /// for which no individual cost was specified,
+    /// unless overridden on initialization.
+    public static var defaultCost: Cost {
+        1
+    }
+
+    /// Creates an empty cache with preallocated space
+    /// for at least the specified number of tokens
+    /// with a `self.defaultCost` of `Self.defaultcost`.
+    ///
+    /// - Note:
+    ///   For performance reasons, the size of the newly allocated
+    ///   storage might be greater than the requested capacity.
+    ///   Use the policy's `capacity` property to determine the size
+    ///   of the new storage.
+    ///
+    /// - Parameters:
+    ///   - minimumCapacity:
+    ///     The requested number of elements to store.
+    ///   - totalCostLimit:
+    ///     The maximum total cost that the cache can
+    ///     hold before it starts evicting objects.
+    public init(
+        minimumCapacity: Int? = nil,
+        totalCostLimit: Cost? = nil
+    ) {
+        self.init(
+            minimumCapacity: minimumCapacity,
+            totalCostLimit: totalCostLimit,
+            defaultCost: Self.defaultCost
+        )
+    }
+}
+
+extension Cache
+where
+    Cost == UInt
+{
+    /// The default cost associated with all stored value,
+    /// for which no individual cost was specified,
+    /// unless overridden on initialization.
+    public static var defaultCost: Cost {
+        1
+    }
+
+    /// Creates an empty cache with preallocated space
+    /// for at least the specified number of tokens
+    /// with a `self.defaultCost` of `Self.defaultcost`.
+    ///
+    /// - Note:
+    ///   For performance reasons, the size of the newly allocated
+    ///   storage might be greater than the requested capacity.
+    ///   Use the policy's `capacity` property to determine the size
+    ///   of the new storage.
+    ///
+    /// - Parameters:
+    ///   - minimumCapacity:
+    ///     The requested number of elements to store.
+    ///   - totalCostLimit:
+    ///     The maximum total cost that the cache can
+    ///     hold before it starts evicting objects.
+    public init(
+        minimumCapacity: Int? = nil,
+        totalCostLimit: Cost? = nil
+    ) {
+        self.init(
+            minimumCapacity: minimumCapacity,
+            totalCostLimit: totalCostLimit,
+            defaultCost: Self.defaultCost
+        )
+    }
+}
+
+extension Cache
+where
+    Cost == Float
+{
+    /// The default cost associated with all stored value,
+    /// for which no individual cost was specified,
+    /// unless overridden on initialization.
+    public static var defaultCost: Cost {
+        1.0
+    }
+
+    /// Creates an empty cache with preallocated space
+    /// for at least the specified number of tokens
+    /// with a `self.defaultCost` of `Self.defaultcost`.
+    ///
+    /// - Note:
+    ///   For performance reasons, the size of the newly allocated
+    ///   storage might be greater than the requested capacity.
+    ///   Use the policy's `capacity` property to determine the size
+    ///   of the new storage.
+    ///
+    /// - Parameters:
+    ///   - minimumCapacity:
+    ///     The requested number of elements to store.
+    ///   - totalCostLimit:
+    ///     The maximum total cost that the cache can
+    ///     hold before it starts evicting objects.
+    public init(
+        minimumCapacity: Int? = nil,
+        totalCostLimit: Cost? = nil
+    ) {
+        self.init(
+            minimumCapacity: minimumCapacity,
+            totalCostLimit: totalCostLimit,
+            defaultCost: Self.defaultCost
+        )
+    }
+}
+
+extension Cache
+where
+    Cost == Double
+{
+    /// The default cost associated with all stored value,
+    /// for which no individual cost was specified,
+    /// unless overridden on initialization.
+    public static var defaultCost: Cost {
+        1.0
+    }
+
+    /// Creates an empty cache with preallocated space
+    /// for at least the specified number of tokens
+    /// with a `self.defaultCost` of `Self.defaultcost`.
+    ///
+    /// - Note:
+    ///   For performance reasons, the size of the newly allocated
+    ///   storage might be greater than the requested capacity.
+    ///   Use the policy's `capacity` property to determine the size
+    ///   of the new storage.
+    ///
+    /// - Parameters:
+    ///   - minimumCapacity:
+    ///     The requested number of elements to store.
+    ///   - totalCostLimit:
+    ///     The maximum total cost that the cache can
+    ///     hold before it starts evicting objects.
+    public init(
+        minimumCapacity: Int? = nil,
+        totalCostLimit: Cost? = nil
+    ) {
+        self.init(
+            minimumCapacity: minimumCapacity,
+            totalCostLimit: totalCostLimit,
+            defaultCost: Self.defaultCost
+        )
+    }
+}
