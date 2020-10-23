72fb01445378fd558decfea45cbd70e255d25097
Vincent Esche <regexident@gmail.com>
2020-10-23 02:06:03 +0200

    Refactored `RRPolicy` to use a dense bitset instead of `Set<Int>`

--- Sources/Cache/RR/RRPolicy.swift
+++ Sources/Cache/RR/RRPolicy.swift
@@ -6,23 +6,23 @@ public typealias RRCache<Key, Value> = CustomCache<Key, Value, Int, RRPolicy>
 where
     Key: Hashable
 
+public typealias RRIndex = ChunkedBitIndex
+
 public struct RRPolicy: CachePolicy {
+    internal typealias Bits = UInt8
+    internal typealias Chunk = BitChunk<Bits>
     public typealias Index = RRIndex
 
     public var isEmpty: Bool {
-        self.indices.isEmpty
-    }
-
-    public var count: Int {
-        self.indices.count
+        self.count == 0
     }
 
     public var capacity: Int {
-        self.indices.capacity
+        self.chunks.capacity * Chunk.bitWidth
     }
 
-    internal private(set) var counter: Int
-    internal private(set) var indices: Set<Index>
+    public private(set) var count: Int
+    internal private(set) var chunks: [Chunk]
 
     public init() {
         self.init(minimumCapacity: 0)
@@ -31,25 +31,80 @@ public struct RRPolicy: CachePolicy {
     public init(minimumCapacity: Int) {
         assert(minimumCapacity >= 0)
 
+        // Next smallest greater than or equal power of 2:
+        let capacity: Int
+
+        if minimumCapacity > 0 {
+            let leadingZeros = minimumCapacity.leadingZeroBitCount
+            capacity = 0b1 << (Int.bitWidth - leadingZeros)
+        } else {
+            capacity = 0
+        }
+
+        // Calculate required number of chunks, rounding up:
+        let chunkCount = Self.chunksFor(count: capacity)
+
+        let chunks: [Chunk] = Array(
+            repeating: .init(),
+            count: chunkCount
+        )
+
         self.init(
-            counter: 0,
-            indices: .init(minimumCapacity: minimumCapacity)
+            count: 0,
+            chunks: chunks
         )
     }
 
     internal init(
-        counter: Int,
-        indices: Set<Index>
+        count: Int,
+        chunkBits: [Bits]
     ) {
-        self.counter = counter
-        self.indices = indices
+        self.init(
+            count: count,
+            chunks: chunkBits.map { .init(bits: $0) }
+        )
+    }
+
+    internal init(
+        count: Int,
+        chunks: [Chunk]
+    ) {
+        self.count = count
+        self.chunks = chunks
     }
 
     public mutating func insert() -> Index {
-        let index = Index(self.counter)
+        if self.count == self.chunks.count * Chunk.bitWidth {
+            self.chunks.append(.init())
+        }
 
-        self.counter += 1
-        self.indices.insert(index)
+        let startIndex = self.randomStartIndex()
+        var (chunkIndex, startBitIndex) = startIndex.indices(
+            given: Bits.self
+        )
+
+        // Skip over full chunks:
+        for _ in 0...self.chunks.count {
+            let mask = Chunk.mask(range: startBitIndex..<Chunk.bitWidth)
+            guard self.chunks[chunkIndex].isOnes(atMask: mask) else {
+                break
+            }
+            startBitIndex = 0
+            chunkIndex += 1
+        }
+
+        let chunk = self.chunks[chunkIndex]
+        let range = startBitIndex..<Bits.bitWidth
+
+        let endBitIndexOrNil = chunk.indexOfFirstZero(inRange: range)
+        guard let endBitIndex = endBitIndexOrNil else {
+            fatalError("Expected vacancy, found none")
+        }
+
+        let mask = Chunk.mask(range: startBitIndex...endBitIndex)
+        self.chunks[chunkIndex].setOnes(atMask: mask)
+        let index = Self.index(chunk: chunkIndex, bit: endBitIndex)
+        self.count += 1
 
         return index
     }
@@ -59,18 +114,42 @@ public struct RRPolicy: CachePolicy {
     }
 
     public mutating func remove() -> Index? {
-        guard !self.indices.isEmpty else {
-            return nil
+        let startIndex = self.randomStartIndex()
+        var (chunkIndex, startBitIndex) = startIndex.indices(
+            given: Bits.self
+        )
+
+        // Skip over full chunks:
+        for _ in 0...self.chunks.count {
+            let mask = Chunk.mask(range: startBitIndex..<Chunk.bitWidth)
+            guard self.chunks[chunkIndex].isZeros(atMask: mask) else {
+                break
+            }
+            startBitIndex = 0
+            chunkIndex += 1
         }
-        // Calling `.removeFirst()` on Swift's Set
-        // should be reasonably close to random here.
-        // The order of
-        return self.indices.removeFirst()
+
+        let chunk = self.chunks[chunkIndex]
+        let range = startBitIndex..<Bits.bitWidth
+
+        let endBitIndexOrNil = chunk.indexOfFirstOne(inRange: range)
+        guard let endBitIndex = endBitIndexOrNil else {
+            fatalError("Expected occupancy, found none")
+        }
+
+        let mask = Chunk.mask(range: startBitIndex...endBitIndex)
+        self.chunks[chunkIndex].setZeros(atMask: mask)
+        let index = Self.index(chunk: chunkIndex, bit: endBitIndex)
+        self.count -= 1
+
+        return index
     }
 
     public mutating func remove(_ index: Index) {
-        assert(self.indices.contains(index))
-        self.indices.remove(index)
+        let (chunkIndex, bitIndex) = index.indices(given: Bits.self)
+        let mask = Chunk.mask(index: bitIndex)
+        self.chunks[chunkIndex].setZeros(atMask: mask)
+        self.count -= 1
     }
 
     @inlinable
@@ -82,7 +161,42 @@ public struct RRPolicy: CachePolicy {
     public mutating func removeAll(
         keepingCapacity keepCapacity: Bool
     ) {
-        self.counter = 0
-        self.indices.removeAll(keepingCapacity: keepCapacity)
+        self.count = 0
+        self.chunks.removeAll(keepingCapacity: keepCapacity)
+    }
+
+    private static func chunksFor(count: Int) -> Int {
+        let bitWidth = Chunk.bitWidth
+        // Calculate required number of chunks, by rounding up:
+        return (count + (bitWidth - 1)) / bitWidth
+    }
+
+    private static func index(chunk: Int, bit: Int) -> Index {
+        .init(
+            given: Bits.self,
+            chunkIndex: chunk,
+            bitIndex: bit
+        )
+    }
+
+    private func randomStartIndex() -> Index {
+        var prng = SplitMix64(state: .init(self.count))
+        let uint = UInt(truncatingIfNeeded: prng.next())
+        let int = Int(truncatingIfNeeded: uint >> 1)
+        let count = self.count
+        let index = (count != 0) ? (int % count) : 0
+        return .init(absoluteBitIndex: index)
+    }
+
+    private func isValid() -> Bool {
+        let validCount = self.chunks.reduce(0) {
+            $0 + $1.count
+        } == self.count
+
+        guard validCount else {
+            return false
+        }
+
+        return true
     }
 }
