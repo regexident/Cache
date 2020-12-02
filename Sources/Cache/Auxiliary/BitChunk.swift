005d407fde41b39d6c1b58b13dc6a238fca48806
Vincent Esche <regexident@gmail.com>
2020-12-02 01:10:38 +0100

    Marked API of `BitChunk<T>` as always inlineable

--- Sources/Cache/Auxiliary/BitChunk.swift
+++ Sources/Cache/Auxiliary/BitChunk.swift
@@ -6,56 +6,84 @@ internal struct BitChunk<Bits>
 where
     Bits: FixedWidthInteger
 {
+    @inlinable
+    @inline(__always)
     internal static var bitWidth: Int {
         Bits.bitWidth
     }
 
+    @inlinable
+    @inline(__always)
     internal static var zeros: Self {
         .init(bits: 0b0 as Bits)
     }
 
+    @inlinable
+    @inline(__always)
     internal static var ones: Self {
         .init(bits: ~0b0 as Bits)
     }
 
+    @inlinable
+    @inline(__always)
     internal var isEmpty: Bool {
         self == .zeros
     }
 
+    @inlinable
+    @inline(__always)
     internal var isFull: Bool {
         self == .ones
     }
 
+    @inlinable
+    @inline(__always)
     internal var count: Int {
         self.bits.nonzeroBitCount
     }
 
+    @usableFromInline
+    @inline(__always)
     internal private(set) var bits: Bits
 
+    @inlinable
+    @inline(__always)
     internal init() {
         self.init(bits: 0b0)
     }
 
+    @inlinable
+    @inline(__always)
     internal init(bits: Bits) {
         self.bits = bits
     }
 
+    @inlinable
+    @inline(__always)
     internal static func emptyRange() -> Range<Int> {
         0..<0
     }
 
+    @inlinable
+    @inline(__always)
     internal static func fullRange() -> Range<Int> {
         0..<Bits.bitWidth
     }
 
+    @inlinable
+    @inline(__always)
     internal static func emptyMask() -> Bits {
         (0b0 as Bits)
     }
 
+    @inlinable
+    @inline(__always)
     internal static func fullMask() -> Bits {
         ~(0b0 as Bits)
     }
 
+    @inlinable
+    @inline(__always)
     internal static func mask(
         index: Int
     ) -> Bits {
@@ -63,6 +91,8 @@ where
         return (0b1 as Bits) &<< index
     }
 
+    @inlinable
+    @inline(__always)
     internal static func mask(
         range: Range<Int>
     ) -> Bits {
@@ -75,6 +105,8 @@ where
         return bits
     }
 
+    @inlinable
+    @inline(__always)
     internal static func mask(
         range: ClosedRange<Int>
     ) -> Bits {
@@ -87,6 +119,8 @@ where
         return bits
     }
 
+    @inlinable
+    @inline(__always)
     internal static func mask(
         range: PartialRangeFrom<Int>
     ) -> Bits {
@@ -96,6 +130,8 @@ where
         return bits
     }
 
+    @inlinable
+    @inline(__always)
     internal static func mask(
         range: PartialRangeUpTo<Int>
     ) -> Bits {
@@ -105,6 +141,8 @@ where
         return bits
     }
 
+    @inlinable
+    @inline(__always)
     internal static func mask(
         range: PartialRangeThrough<Int>
     ) -> Bits {
@@ -113,36 +151,52 @@ where
         return bits
     }
 
+    @inlinable
+    @inline(__always)
     internal mutating func setZeros(atMask mask: Bits) {
         self.bits &= ~mask
     }
 
+    @inlinable
+    @inline(__always)
     internal mutating func setOnes(atMask mask: Bits) {
         self.bits |= mask
     }
 
+    @inlinable
+    @inline(__always)
     internal func isZeros(atMask mask: Bits) -> Bool {
         (self.bits & mask) == Self.emptyMask()
     }
 
+    @inlinable
+    @inline(__always)
     internal func isOnes(atMask mask: Bits) -> Bool {
         (self.bits & mask) == mask
     }
 
+    @inlinable
+    @inline(__always)
     internal func hasZeros(atMask mask: Bits) -> Bool {
         (self.bits & mask) != mask
     }
 
+    @inlinable
+    @inline(__always)
     internal func hasOnes(atMask mask: Bits) -> Bool {
         (self.bits & mask) != Self.emptyMask()
     }
 
+    @inlinable
+    @inline(__always)
     internal func indexOfFirstZero(
         inRange range: Range<Int>
     ) -> Int? {
         Self.indexOfFirstOne(in: ~self.bits, range: range)
     }
 
+    @inlinable
+    @inline(__always)
     internal func indexOfFirstOne(
         inRange range: Range<Int>
     ) -> Int? {
